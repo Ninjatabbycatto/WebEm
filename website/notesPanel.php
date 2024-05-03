749fe21ad24aae86981748211529bdbcc0c95d7c
Ninjatabbycatto <131937596+Ninjatabbycatto@users.noreply.github.com>
2024-05-04 02:08:13 +0800

    notes panel

---
+++ website/notesPanel.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/notesPanel.css">
+    <link rel="stylesheet" href="css/Fonts/roboto.css">
+
+    <scripts>
+
+    </scripts>
+
+  </head>
+
+  <body>
+    <div class = "rightPanelRectangle">
+        <div class = "rowContainer">
+            <div class='titleContainer'><h1 class = 'title'>Title</h1></div>
+            <div class='noteDetailContainer'>
+                <h1 class='noteTitle roboto-bold'>Note 1</h1>
+                <p class='noteDescription roboto-light'>Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit. Curabitur  elementum neque non ligula gravida, vitae 
+                </p>
+            </div>
+
+        </div>
+        <div class = "rowContainer2">
+            <div class='titleContainer'><h1 class = 'title2'>Title</h1></div>
+            <div class='actualNote'>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur  elementum neque non ligula gravida, vitae 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur  elementum neque non ligula gravida, vitae 
+                </p>
+            </div>
+        </div>
+       
+    </div>
+  </body>
